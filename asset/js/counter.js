3796f45d2e0eae3ab4ef2f2a1e6d00bd67cc2815
Michele del Giudice <dgmichele96@gmail.com>
2024-09-18 09:49:47 +0200

    v2

--- asset/js/counter.js
+++ asset/js/counter.js
@@ -1,37 +1,61 @@
-// Qui cerchiamo e otteniamo gli elementi del contatore dal DOM
-const displayContatore = document.querySelector('[data-contatore]');
-const bottoneIncremento = document.querySelector('[data-incremento]');
-const bottoneDecremento = document.querySelector('[data-decremento]');
-const bottoneReset = document.querySelector('[data-reset]'); 
+// ✏️ Prima di tutto dobbiamo cercare e ottenere il contenitore del display e il contenitore dei pulsanti dal DOM, per poi appendere al primo il relativo valore e al secondo i relativi pulsanti, dopodiché dobbiamo assegnare ad una variabile il valore iniziale del display, che ovviamente sarà uguale a 0:
 
-let risultato = 0;  // Qui memorizziamo nella variabile il valore iniziale del contatore, che sarà ovviamente a 0
+// Qui cerchiamo e otteniamo il contenitore del display e il contenitore dei pulsanti dal DOM
+const displayContatore = document.querySelector('.contatore'); // contenitore display
+const pulsantiContainer = document.querySelector('.pulsanti'); // contenitore pulsanti
 
-// Qui gestiamo l'incremento del pulsante "+"
+// Qui memorizziamo il valore iniziale del contatore nella variabile "risultato" e poi creiamo il testo dinamico per i numeri da mettere all'interno del display
+let risultato = 0;
+displayContatore.innerText = risultato;  // testo del valore iniziale all'interno di <div class="contatore">
+
+// ✏️ Ora dobbiamo creare i pulsanti dinamici da appendere al contenitore dei pulsanti:
+
+// Qui creiamo il pulsante di decremento
+const bottoneDecremento = document.createElement('button');
+bottoneDecremento.className = 'decremento';
+bottoneDecremento.innerText = '-';
+pulsantiContainer.append(bottoneDecremento); // appendiamo il nuovo bottone all'interno di <div class="pulsanti">
+
+// Qui creiamo il pulsante di reset
+const bottoneReset = document.createElement('button');
+bottoneReset.className = 'reset';
+bottoneReset.innerText = 'Reset';
+pulsantiContainer.append(bottoneReset); // appendiamo il nuovo bottone all'interno di <div class="pulsanti">
+
+// Qui creiamo il pulsante di incremento
+const bottoneIncremento = document.createElement('button');
+bottoneIncremento.className = 'incremento';
+bottoneIncremento.innerText = '+';
+pulsantiContainer.append(bottoneIncremento); // appendiamo il nuovo bottone all'interno di <div class="pulsanti">
+
+// ✏️ Ora dobbiamo rendere il valore del numero nel display dinamico:
+
+// Qui gestiamo l'incremento del contatore
 bottoneIncremento.addEventListener('click', function() {
-    risultato++; 
-    displayContatore.innerText = risultato;  // Aggiorniamo il display dopo l'incremento della variabile "risultato"
+    risultato++;
+    displayContatore.innerText = risultato;
     colore();
 });
 
-// Qui gestiamo il decremento del pulsante "-"
+// Qui gestiamo il decremento del contatore
 bottoneDecremento.addEventListener('click', function() {
     risultato--;
-    displayContatore.innerText = risultato;  // Aggiorniamo il display dopo il decremento della variabile "risultato"
+    displayContatore.innerText = risultato;
     colore();
 });
 
-// Qui gestiamo il reset dei valori immessi in precedenza con il tasto "reset"
+// Qui gestiamo il reset del contatore
 bottoneReset.addEventListener('click', function() {
-    risultato = 0;  // Reimpostiamo il valore del contatore a 0
-    displayContatore.innerText = risultato;  // Aggiorniamo il display dopo il reset, ossia la variabile "risultato" prende il valore 0
+    risultato = 0;
+    displayContatore.innerText = risultato;
     colore();
 });
 
-function colore(){
-    displayContatore.innerText = risultato; // prendiamo il valore nel display aggiornato
+// ✏️ Ora dobbiamo rendere condizionali i colori (se positivi verdi, se negativi rossi, uguale a 0 sarà invariato)
 
-    // cambiamo colore se il numero è positivo o negativo, invece se è uguale a 0 rimane invariato
-    if(risultato > 0){
+// Qui creiamo la funzione per cambiare il colore del display in base al valore del contatore
+function colore() {
+    if (risultato > 0) {
         displayContatore.style.color = '#2fa142';
     } else if (risultato < 0) {
         displayContatore.style.color = '#d63131';
